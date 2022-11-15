9ffad7e371cca287c2266d317b692401d400fb75
Dmitry Mudry <d.mudryi@intabia.ru>
2022-11-15 14:24:55 +0700

    carcass

---
+++ src/main/java/com/intabia/grpclecture/GrpcLectureApplication.java
@@ -0,0 +1,11 @@
+package com.intabia.grpclecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrpcLectureApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GrpcLectureApplication.class, args);
+	}
+}
---
+++ src/test/java/com/intabia/grpclecture/GrpcLectureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.intabia.grpclecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrpcLectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
